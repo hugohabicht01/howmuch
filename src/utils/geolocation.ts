86037808e1b030261a67c83dae68cd1eb6bac5c1
hugohabicht01 <anon@example.org>
2022-09-09 15:39:39 +0200

    feat: use geolocation watcher instead of the geolocation callback API

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 interface LocationFoundType {
   coords: GeolocationPosition
@@ -20,16 +20,82 @@ interface LocationLoadingType {
 
 export type LocationStateType = LocationFoundType | LocationErrorType | LocationLoadingType
 
-export const useGeolocation = () => {
-  const [location, setLocation] = useState<LocationStateType>({ coords: null, error: null, isLoading: true })
+// export const useGeolocation = () => {
+//   const [location, setLocation] = useState<LocationStateType>({ coords: null, error: null, isLoading: true })
 
-  const locationSuccessHandler = (pos: GeolocationPosition) => setLocation({ coords: pos, error: null, isLoading: false })
-  const locationErrorHandler = (err: GeolocationPositionError) => setLocation({ coords: null, error: err, isLoading: false })
+//   const locationSuccessHandler = (pos: GeolocationPosition) => setLocation({ coords: pos, error: null, isLoading: false })
+//   const locationErrorHandler = (err: GeolocationPositionError) => setLocation({ coords: null, error: err, isLoading: false })
 
-  useEffect(() => {
-    if ('geolocation' in navigator)
-      navigator.geolocation.getCurrentPosition(locationSuccessHandler, locationErrorHandler)
+//   useEffect(() => {
+//     if ('geolocation' in navigator)
+//       navigator.geolocation.getCurrentPosition(locationSuccessHandler, locationErrorHandler)
+//   })
+
+//   return location
+// }
+
+export const useGeolocation = (
+  { enableHighAccuracy, maximumAge, timeout }: PositionOptions = {},
+  callback: (...args: any) => any,
+  isEnabled = true,
+) => {
+  const [coordinates, setCoordinates] = useState<LocationStateType>({
+    isLoading: true,
+    coords: null,
+    error: null,
   })
 
-  return location
+  const updateCoordinates = useCallback<PositionCallback>(
+    (pos) => {
+      setCoordinates({
+        coords: pos,
+        isLoading: false,
+        error: null,
+      })
+
+      if (typeof callback === 'function')
+        callback(coordinates)
+    },
+    [callback, coordinates],
+  )
+
+  const setError = useCallback<PositionErrorCallback>((error) => {
+    setCoordinates({
+      coords: null,
+      isLoading: false,
+      error,
+    })
+  }, [])
+
+  useEffect(() => {
+    let watchId: number
+
+    if (isEnabled && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(updateCoordinates, setError)
+      watchId = navigator.geolocation.watchPosition(
+        updateCoordinates,
+        setError,
+        {
+          enableHighAccuracy,
+          maximumAge,
+          timeout,
+        },
+      )
+    }
+
+    return () => {
+      if (watchId)
+        navigator.geolocation.clearWatch(watchId)
+    }
+  }, [
+    isEnabled,
+    callback,
+    enableHighAccuracy,
+    maximumAge,
+    setError,
+    timeout,
+    updateCoordinates,
+  ])
+
+  return coordinates
 }
