4dbd0fa939bf192931ce95be3ca165234434c698
hugohabicht01 <anon@example.org>
2022-09-13 23:20:17 +0200

    feat: add debounce logic for geolocation

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,18 +1,19 @@
 import { useCallback, useEffect, useState } from 'react'
+import haversine from 'haversine'
 
-interface LocationFoundType {
+export interface LocationFoundType {
   coords: GeolocationPosition
   error: null
   isLoading: false
 }
 
-interface LocationErrorType {
+export interface LocationErrorType {
   coords: null
   error: GeolocationPositionError
   isLoading: false
 }
 
-interface LocationLoadingType {
+export interface LocationLoadingType {
   coords: null
   error: null
   isLoading: true
@@ -20,23 +21,9 @@ interface LocationLoadingType {
 
 export type LocationStateType = LocationFoundType | LocationErrorType | LocationLoadingType
 
-// export const useGeolocation = () => {
-//   const [location, setLocation] = useState<LocationStateType>({ coords: null, error: null, isLoading: true })
-
-//   const locationSuccessHandler = (pos: GeolocationPosition) => setLocation({ coords: pos, error: null, isLoading: false })
-//   const locationErrorHandler = (err: GeolocationPositionError) => setLocation({ coords: null, error: err, isLoading: false })
-
-//   useEffect(() => {
-//     if ('geolocation' in navigator)
-//       navigator.geolocation.getCurrentPosition(locationSuccessHandler, locationErrorHandler)
-//   })
-
-//   return location
-// }
-
 export const useGeolocation = (
   { enableHighAccuracy, maximumAge, timeout }: PositionOptions = {},
-  callback: (...args: any) => any,
+  callback: ((newLocation: LocationStateType) => void) | undefined,
   isEnabled = true,
 ) => {
   const [coordinates, setCoordinates] = useState<LocationStateType>({
@@ -98,4 +85,48 @@ export const useGeolocation = (
   ])
 
   return coordinates
+}
+
+/**
+ * Debounces geolocation values according to a distance threshold
+ * This is done to avoid recalculations and rerendering after very small coordinate changes
+ */
+export const geolocationDebouncer = (oldState: LocationStateType, newState: LocationStateType, distanceThreshold: number): { state: LocationStateType; modified: boolean } => {
+  // Get the newest error instead of the old one
+  if (!newState.coords && !oldState.coords)
+    return { modified: true, state: newState }
+
+  // TODO: Add some checks how old the good data is
+  // Don't bother returning error if we still have old good data that can be used
+  if (!newState.coords)
+    return { state: oldState, modified: false }
+  // If we didn't have any data previously, returning the new one is a no brainer
+  if (!oldState.coords)
+    return { state: newState, modified: true }
+
+  const newCoords = { lat: newState.coords.coords.latitude, lng: newState.coords.coords.longitude }
+  const oldCoords = { lat: oldState.coords.coords.latitude, lng: oldState.coords.coords.longitude }
+  const distance = haversine(newCoords, oldCoords, { format: '{lat,lng}', unit: 'meter' })
+  if (distance > distanceThreshold)
+    return { state: newState, modified: true }
+
+  return { state: oldState, modified: false }
+}
+
+export const useDebouncedGeolocation = (distanceThreshold: number, isEnabled: boolean) => {
+  const [debounced, setDebounced] = useState<LocationStateType>({
+    isLoading: true,
+    coords: null,
+    error: null,
+  })
+
+  const onLocationChanged = (newLocation: LocationStateType) => {
+    const { state, modified } = geolocationDebouncer(debounced, newLocation, distanceThreshold)
+    if (modified)
+      setDebounced(state)
+  }
+
+  useGeolocation({ enableHighAccuracy: true, timeout: 1000 * 60, maximumAge: 1000 * 60 * 10 }, onLocationChanged, isEnabled)
+
+  return debounced
 }
